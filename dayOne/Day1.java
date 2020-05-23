fb4711ad63ae2111f245b1e772887cfcc3e0b81e
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-23 10:31:53 +0530

    Completed Day One

---
+++ dayOne/Day1.java
@@ -0,0 +1,28 @@
+package dayOne;
+
+import java.util.Scanner;
+
+public class Day1 {
+    
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+        int j = scan.nextInt();
+        double e = scan.nextDouble();
+        scan.nextLine();
+        String str = scan.nextLine();
+
+        System.out.println(i+j);
+
+        System.out.printf("%.1f\n",d+e);
+
+        System.out.println(s+str);
+
+        scan.close();
+    }
+
+}
