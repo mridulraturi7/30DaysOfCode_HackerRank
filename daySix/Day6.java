8aa589df0bf58f1264764482b574ff642f72e3a8
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-28 11:25:48 +0530

    Modified Day6.java

--- daySix/Day6.java
+++ daySix/Day6.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Day6 {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-
+        
     Scanner scan = new Scanner(System.in);
     int n = scan.nextInt();
 
@@ -17,6 +16,8 @@ public class Day6 {
         System.out.println();
         n--;
     }
+
+    scan.close();
 
     }
 
