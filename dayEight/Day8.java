2055c523c244df7549e52f98d5c28525dd0d9799
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-30 12:01:33 +0530

    Removed unnecessary comments from Day8.java

--- dayEight/Day8.java
+++ dayEight/Day8.java
@@ -12,15 +12,15 @@ public class Day8 {
 
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
             phoneBook.put(name,phone);
-            // Write code here
         }
+
         while(in.hasNext()){
             String s = in.next();
-            // Write code here
             if(phoneBook.containsKey(s))
             {
                 System.out.printf("%s=%d\n",s,phoneBook.get(s));
