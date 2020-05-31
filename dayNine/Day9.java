db334ee527449130223012ced9a2f67ba41a9808
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-31 18:23:43 +0530

    Updated Day9.java

--- dayNine/Day9.java
+++ dayNine/Day9.java
@@ -14,19 +14,20 @@ public class Day9 {
 
         return n * factorial(n-1);
 
-}
+    }
 
-private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
-public static void main(String[] args) throws IOException {
-    int n = scanner.nextInt();
-    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    public static void main(String[] args) throws IOException {
+     
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-    int result = factorial(n);
+        int result = factorial(n);
 
-    System.out.println(result);
+        System.out.println(result);
 
-    scanner.close();
-}
-    
+        scanner.close();
+    }
+
 }
