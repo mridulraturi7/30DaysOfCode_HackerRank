a5c55751e432210cfc3ac610c05c02a7f6b0b748
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-31 18:25:12 +0530

    Minor Change in Day9.java

--- dayNine/Day9.java
+++ dayNine/Day9.java
@@ -16,9 +16,9 @@ public class Day9 {
 
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
      
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
