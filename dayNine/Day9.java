f5a56f92761c22c87e9fa4ac0a1e5c0654dd8a8d
mridulraturi7 <mridulraturi7@gmail.com>
2020-05-31 18:21:56 +0530

    Completed Day Nine

--- dayNine/Day9.java
+++ dayNine/Day9.java
@@ -1,5 +1,37 @@
 package dayNine;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Day9 {
+
+    static int factorial(int n) {
+
+        if(n <= 1)
+        {
+            return 1;
+        }
+
+        return n * factorial(n-1);
+
+}
+
+private static final Scanner scanner = new Scanner(System.in);
+
+public static void main(String[] args) throws IOException {
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int n = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    int result = factorial(n);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedWriter.close();
+
+    scanner.close();
+}
     
 }
