50bb55543d0c44a42bde8c23f71205ca2f470b4f
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-03 11:23:08 +0530

    Completed Day Twelve

--- dayTwelve/Day12.java
+++ dayTwelve/Day12.java
@@ -25,25 +25,12 @@ class Student extends Person{
 
 	private int[] testScores;
 
-    /*	
-    *   Class Constructor
-    *   
-    *   @param firstName - A string denoting the Person's first name.
-    *   @param lastName - A string denoting the Person's last name.
-    *   @param id - An integer denoting the Person's ID number.
-    *   @param scores - An array of integers denoting the Person's test scores.
-    */
-    // Write your constructor here
     Student(String firstName, String lastName, int id, int[] scores)
     {
         super(firstName, lastName, id);
         this.testScores = scores;
     }
-    /*	
-    *   Method Name: calculate
-    *   @return A character denoting the grade.
-    */
-    // Write your method here
+
     public char calculate()
     {
         int sum=0,average;
