f633e49ac5aebfbf90a124c2ba36fdff41e5307e
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-03 11:12:19 +0530

    [Student class Create] Day12.java

--- dayTwelve/Day12.java
+++ dayTwelve/Day12.java
@@ -23,6 +23,62 @@ class Person {
 	 
 }
 
+class Student extends Person{
+    
+	private int[] testScores;
+    private String fname;
+    private String lname;
+    private int id;
+
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+    Student(String firstName, String lastName, int id, int[] scores)
+    {
+        super(firstName, lastName, id);
+        this.testScores = scores;
+    }
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+    public char calculate()
+    {
+        int sum=0,average;
+        for(int i=0; i<testScores.length; i++)
+        {
+            sum = sum + testScores[i];
+        }
+
+        average = sum/testScores.length;
+
+        if(average>=90 && average<=100)
+            return 'O';
+
+        if(average>=80 && average<90)
+            return 'E';
+
+        if(average>=70 && average<80)
+            return 'A';
+
+        if(average>=55 && average<70)
+            return 'P';
+
+        if(average>=40 && average<55)
+            return 'D';
+
+        return 'T';
+    }
+   
+}
+
 public class Day12
 {
     public static void main(String[] args) {
