0d046fc3306c244285392604f770f3e0684bf00a
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-04 11:19:51 +0530

    [Create Main method] Day13.java

--- dayThirteen/Day13.java
+++ dayThirteen/Day13.java
@@ -2,6 +2,15 @@ package dayThirteen;
 
 public class Day13 {
 
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }    
     
 }
