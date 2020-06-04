dd7e42ae3759d16132c81d914594fb5e100c7dca
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-04 11:22:17 +0530

    [Create MyBook class extending Book class] Day13.java

--- dayThirteen/Day13.java
+++ dayThirteen/Day13.java
@@ -12,6 +12,24 @@ abstract class Book {
     abstract void display();
 }
 
+class MyBook extends Book
+{
+    int price;
+
+    MyBook(String title, String author, int price)
+    {
+        super(title, author);
+        this.price = price;
+    }
+
+    void display()
+    {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
+
 import java.util.Scanner;
 
 public class Day13 {
