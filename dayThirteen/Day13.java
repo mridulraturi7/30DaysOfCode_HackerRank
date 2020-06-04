cc038f321a9e910a2fbf0007c843fdf233c53b42
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-04 11:21:19 +0530

    [Create Book class] Day13.java

--- dayThirteen/Day13.java
+++ dayThirteen/Day13.java
@@ -1,5 +1,17 @@
 package dayThirteen;
 
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+
 import java.util.Scanner;
 
 public class Day13 {
