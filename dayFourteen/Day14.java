54047a2496bfbd592930898bbab9554c6a62ad56
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-05 18:06:57 +0530

    [Create main method] Day14.java

--- dayFourteen/Day14.java
+++ dayFourteen/Day14.java
@@ -2,6 +2,20 @@ package dayFourteen;
 
 public class Day14 {
 
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
     
 }
