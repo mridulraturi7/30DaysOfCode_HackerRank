f4531f3870cd03dfec09ab5433bd46c748cb61f0
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-07 18:15:36 +0530

    Completed Day Sisteen

--- daySixteen/Day16.java
+++ daySixteen/Day16.java
@@ -2,6 +2,17 @@ package daySixteen;
 
 public class Day16 {
 
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try
+        {
+            int i = Integer.parseInt(S);
+            System.out.println(i);
+        }catch(Exception e)
+        {
+            System.out.println("Bad String");
+        }
+    }
     
 }
