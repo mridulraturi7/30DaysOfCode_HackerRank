54efa019e52ea28ceb7ca4e08089ca450e22d673
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-09 12:08:23 +0530

    Completed Day Eighteen

--- dayEighteen/Day18.java
+++ dayEighteen/Day18.java
@@ -7,7 +7,6 @@ import java.util.LinkedList;
 
 public class Day18 {
 
-        // Write your code here.
         Stack<Character> stack = new Stack<Character>();
         Queue<Character> queue = new LinkedList<>();
     
@@ -35,19 +34,15 @@ public class Day18 {
         String input = scan.nextLine();
         scan.close();
 
-        // Convert input String to an array of characters:
         char[] s = input.toCharArray();
 
-        // Create a Solution object:
         Day18 d = new Day18();
 
-        // Enqueue/Push all chars to their respective data structures:
         for (char c : s) {
             d.pushCharacter(c);
             d.enqueueCharacter(c);
         }
 
-        // Pop/Dequeue the chars at the head of both data structures and compare them:
         boolean isPalindrome = true;
         for (int i = 0; i < s.length/2; i++) {
             if (d.popCharacter() != d.dequeueCharacter()) {
@@ -56,7 +51,6 @@ public class Day18 {
             }
         }
 
-        //Finally, print whether string s is palindrome or not.
         System.out.println( "The word, " + input + ", is " 
                            + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
     }
