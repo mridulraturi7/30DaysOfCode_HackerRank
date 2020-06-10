3884b8cceafcd5085c9d2469a87ccd7a484bb0c8
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-10 18:22:53 +0530

    [Create main method] Day19.java

--- dayNineteen/Day19.java
+++ dayNineteen/Day19.java
@@ -2,6 +2,15 @@ package dayNineteen;
 
 public class Day19 {
 
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
     
 }
