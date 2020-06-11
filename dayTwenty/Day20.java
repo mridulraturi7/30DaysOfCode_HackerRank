6f5ced73a93f3521e532db9c7944d6dc8a9ee0fd
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-11 18:14:04 +0530

    [Create main method] Day20.java

--- dayTwenty/Day20.java
+++ dayTwenty/Day20.java
@@ -2,6 +2,40 @@ package dayTwenty;
 
 public class Day20 {
 
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+
+        int arraySwaps = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            //arraySwaps = 0;
+            for( int j = 0; j < n-1; j++)
+            {
+                if(a[j] > a[j+1])
+                {
+                    int temp;
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    arraySwaps++;
+                }
+            }
+            if(arraySwaps == 0)
+            {
+                break;
+            }
+        }
+
+        System.out.println("Array is sorted in " + arraySwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n-1]);
+    }
     
 }
