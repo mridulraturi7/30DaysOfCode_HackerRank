3df45fc799844335a0a0bcb92c2e536df3330070
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-14 23:37:40 +0530

    [Implemented Logic] Day23

--- dayTwentyThree/Day23.java
+++ dayTwentyThree/Day23.java
@@ -13,6 +13,31 @@ class Node{
 
 public class Day23 {
 
+    static void levelOrder(Node root){
+        //Write your code here'
+        Queue<Node> q = new LinkedList<>();
+        if(root != null)
+        {
+          q.add(root);
+        }
+  
+        while(!q.isEmpty())
+        {
+            Node n = q.remove();
+            System.out.print(n.data + " ");
+  
+            if(n.left != null)
+            {
+                q.add(n.left);
+            }
+  
+            if(n.right != null)
+            {
+                q.add(n.right);
+            }
+        }    
+    }  
+
     public static Node insert(Node root,int data){
         if(root==null){
             return new Node(data);
