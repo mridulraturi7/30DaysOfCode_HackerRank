7e2515639c9f20a9f3bfa92d0043da6bd18d2a2f
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-15 12:22:07 +0530

    [Insert Function Created] Day23.java

--- dayTwentyFour/Day24.java
+++ dayTwentyFour/Day24.java
@@ -13,6 +13,24 @@ class Node{
 
 public class Day24 {
 
+    public static  Node insert(Node head,int data)
+    {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+    }
+
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
