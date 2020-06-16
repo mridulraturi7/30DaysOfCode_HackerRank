65e2df82dbfcdcda62b68e377fb5cec8fd6b3c99
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-16 13:23:32 +0530

    [Complete] Day25.java

--- dayTwentyFive/Day25.java
+++ dayTwentyFive/Day25.java
@@ -27,9 +27,27 @@ public class Day25
                 System.out.println("Not prime");
             }
         }
-
-
     }
+
+    static boolean isPrime(int n) 
+{ 
+    // Corner cases 
+    if (n <= 1) 
+        return false; 
+    if (n <= 3) 
+        return true; 
+  
+    // This is checked so that we can skip 
+    // middle five numbers in below loop 
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (int i = 5; i * i <= n; i = i + 6) 
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return false; 
+  
+    return true; 
+} 
 
 
 }
