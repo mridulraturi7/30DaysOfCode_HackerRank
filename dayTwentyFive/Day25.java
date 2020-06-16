2c93445acdbd535bfac610a2a372a52184ffd7ff
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-16 13:22:22 +0530

    [Create main method] Day25.java

--- dayTwentyFive/Day25.java
+++ dayTwentyFive/Day25.java
@@ -3,6 +3,33 @@ package dayTwentyFive;
 public class Day25
 {
 
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+
+        while(t>0)
+        {
+            boolean check;
+            int n = scan.nextInt();
+            check = isPrime(n);
+            t--;
+
+            if(check == true)
+            {
+                System.out.println("Prime");
+            }
+
+            else if(check == false)
+            {
+                System.out.println("Not prime");
+            }
+        }
+
+
+    }
+
 
 }
