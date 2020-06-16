8a47ed304cf8a51ad6468cb9f4fb40bb47a75283
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-16 13:26:27 +0530

    [Modify] Day25.java

--- dayTwentyFive/Day25.java
+++ dayTwentyFive/Day25.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Day25
 {
-
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
@@ -33,24 +32,22 @@ public class Day25
     }
 
     static boolean isPrime(int n) 
-{ 
-    // Corner cases 
-    if (n <= 1) 
-        return false; 
-    if (n <= 3) 
-        return true; 
+    { 
+        // Corner cases 
+        if (n <= 1) 
+            return false; 
+        if (n <= 3) 
+            return true; 
   
-    // This is checked so that we can skip 
-    // middle five numbers in below loop 
-    if (n % 2 == 0 || n % 3 == 0) 
-        return false; 
-  
-    for (int i = 5; i * i <= n; i = i + 6) 
-        if (n % i == 0 || n % (i + 2) == 0) 
+        // This is checked so that we can skip 
+        // middle five numbers in below loop 
+        if (n % 2 == 0 || n % 3 == 0) 
             return false; 
   
-    return true; 
-} 
-
-
+        for (int i = 5; i * i <= n; i = i + 6) 
+            if (n % i == 0 || n % (i + 2) == 0) 
+                return false; 
+  
+        return true; 
+    }
 }
