e471d7a01d2592f39f17c590dc65922f3629ca54
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-17 18:09:33 +0530

    [Remove Comment] Day26.java

--- dayTwentySix/Day26.java
+++ dayTwentySix/Day26.java
@@ -6,7 +6,6 @@ public class Day26
 {
     public static void main(String[] args) 
     {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
         int dayR = scan.nextInt();
         int monthR = scan.nextInt();
