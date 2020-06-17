c8322a542c0344416a6ab6591b8fd46db2b773b4
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-17 18:06:18 +0530

    [Create main method] Day26.java

--- dayTwentySix/Day26.java
+++ dayTwentySix/Day26.java
@@ -3,6 +3,15 @@ package dayTwentySix;
 public class Day26
 {
 
-    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int dayR = scan.nextInt();
+        int monthR = scan.nextInt();
+        int yearR = scan.nextInt();
+
+        int dayD = scan.nextInt();
+        int monthD = scan.nextInt();
+        int yearD = scan.nextInt();
 
 }
