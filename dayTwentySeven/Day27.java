87093db5a1337e9c1e40002f724e51cfacfcd606
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-18 11:09:36 +0530

    [ADD method] Day27.java

--- dayTwentySeven/Day27.java
+++ dayTwentySeven/Day27.java
@@ -2,6 +2,19 @@ package dayTwentySeven;
 
 public class Day27 {
 
+    public static int minimum_index(int[] seq) {
+        if (seq.length == 0) {
+            throw new IllegalArgumentException("Cannot get the minimum value index from an empty sequence");
+        }
+        int min_idx = 0;
+        for (int i = 1; i < seq.length; ++i) {
+            if (seq[i] < seq[min_idx]) {
+                min_idx = i;
+            }
+        }
+        return min_idx;
+    }
+
     static class TestDataEmptyArray {
         public static int[] get_array() {
             // complete this function
