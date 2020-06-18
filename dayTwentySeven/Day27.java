5bab36bc380032949244809e9248de598bab1570
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-18 11:03:04 +0530

    [Create main method] Day27.java

--- dayTwentySeven/Day27.java
+++ dayTwentySeven/Day27.java
@@ -3,5 +3,11 @@ package dayTwentySeven;
 public class Day27 {
 
     
-    
+
+    public static void main(String[] args) {
+        TestWithEmptyArray();
+        TestWithUniqueValues();
+        TestWithExactlyTwoDifferentMinimums();
+        System.out.println("OK");
+    }  
 }
