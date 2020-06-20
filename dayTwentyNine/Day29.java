432d6df949d9b26b8a025c18b3f05b5c743114c2
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-20 14:55:35 +0530

    [Add main method] Day29.java

--- dayTwentyNine/Day29.java
+++ dayTwentyNine/Day29.java
@@ -2,6 +2,32 @@ package dayTwentyNine;
 
 public class Day29 {
 
-    
+    int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            int max = 0, and = 0;
+
+            for( int i = 1; i < n; i++)
+            {
+                for( int j = i+1; j <= n; j++)
+                {
+                    and = i & j;
+                    if(and > max && and < k)
+                    {
+                        max = and;
+                    }
+                }
+            }
+
+            System.out.println(max);
+
+        }
     
 }
