2133be7772475020ec6a1f42606de7b44b5926f7
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-20 14:56:54 +0530

    [Minor Change] Day29.java

--- dayTwentyNine/Day29.java
+++ dayTwentyNine/Day29.java
@@ -2,6 +2,7 @@ package dayTwentyNine;
 
 public class Day29 {
 
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         int t = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
