f32a7a60b83af61276917cabfc9043d8d838feef
mridulraturi7 <mridulraturi7@gmail.com>
2020-06-23 00:00:33 +0530

    [Modify] Day13.java

--- dayThirteen/Day13.java
+++ dayThirteen/Day13.java
@@ -7,7 +7,8 @@ abstract class Book
     String title;
     String author;
     
-    Book(String title, String author) {
+    Book(String title, String author) 
+    {
         this.title = title;
         this.author = author;
     }
